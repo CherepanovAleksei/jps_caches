6c9ea09cd121614130603dc4b7cb2a4432ef26a4
Aleksei Cherepanov <mrneumann888@gmail.com>
2020-05-28 11:10:02 +0300

    plugin for 19.3 and higher for detecting cache folder(show it in balloon)

--- src/main/kotlin/InitialActivity.kt
+++ src/main/kotlin/InitialActivity.kt
@@ -1,5 +1,9 @@
 
 import com.intellij.compiler.server.BuildManager
+import com.intellij.notification.NotificationDisplayType
+import com.intellij.notification.NotificationGroup
+import com.intellij.notification.NotificationType
+import com.intellij.notification.Notifications
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.externalSystem.service.project.manage.ExternalProjectsDataStorage
 import com.intellij.openapi.project.Project
@@ -9,8 +13,17 @@ class InitialActivity : StartupActivity {
     private val LOG = Logger.getInstance("jps_caches.InitialActivity")
 
     override fun runActivity(project: Project) {
-        LOG.warn(ExternalProjectsDataStorage.getProjectConfigurationDir(project).toFile().absolutePath)
-        LOG.warn(BuildManager.getInstance().getProjectSystemDirectory(project)?.absolutePath)
-        NewVersionChecker(project, 900000).initTimer()
+        showDebugBalloon(ExternalProjectsDataStorage.getProjectConfigurationDir(project).toFile().absolutePath)
+        showDebugBalloon(BuildManager.getInstance().getProjectSystemDirectory(project)?.absolutePath!!)
+        return
     }
+
+    private val displayId = "KotlinUpdateJPSCachesNotificationDebug"
+    private val notificationGroup = NotificationGroup(displayId, NotificationDisplayType.BALLOON, true)
+
+    private fun showDebugBalloon(string: String) {
+        val notification = notificationGroup.createNotification(string, NotificationType.INFORMATION)
+        Notifications.Bus.notify(notification)
+    }
+
 }
